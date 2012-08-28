c9e1a476d79e5eb3e0d8e2ca90ddb0d256671631
keen80 <keen80@gmail.com>
2012-08-28 09:42:57 +0200

    new activity codes e notifications codes

---
+++ src/main/java/it/antreem/birretta/service/util/NotificationCodes.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.antreem.birretta.service.util;
+
+/**
+ *
+ * @author Stefano
+ */
+public enum NotificationCodes {
+    FRIEND_REQUEST(0);
+    
+    private final int type;
+    
+    NotificationCodes(int type){
+        this.type=type;
+    }
+    
+    public int getType(){
+        return type;
+    }
+            
+}
