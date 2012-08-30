1c45ac4186ba0b67fe06ac74259ae0208bf2982e
morlins <gmorlini@imolinfo.it>
2012-08-30 16:30:54 +0200

    simple version of Badge and Notification

---
+++ src/main/java/it/antreem/birretta/service/model/json/SimpleNotification.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.antreem.birretta.service.model.json;
+
+/**
+ *
+ * @author gmorlini
+ */
+public class SimpleNotification {
+     private String code;
+        private int type;
+        private String msg;
+
+        public String getCode() {
+            return code;
+        }
+
+        public void setCode(String code) {
+            this.code = code;
+        }
+
+        public String getMsg() {
+            return msg;
+        }
+
+        public void setMsg(String msg) {
+            this.msg = msg;
+        }
+
+        public int getType() {
+            return type;
+        }
+
+        public void setType(int type) {
+            this.type = type;
+        }
+}
